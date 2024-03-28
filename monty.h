b7cabc925ed839d9fe31877fde62401fe298d1df
Fiifikev <kevinaryeh02@gmail.com>
2024-03-28 12:26:22 -0500

    header file for the monty project

--- monty.h
+++ monty.h
@@ -35,32 +35,44 @@ char *opcode;
 void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 /**
- * struct lus_s - opcode variable
+ * struct bus_s - opcode variable
  * @arg:agrument value
  * @file:file pointer
- * @info :information
+ * @content:information
  * @lifi :flag change
  * Description: value ofprogram LIFO,FIFO
  */
-typedef struct lus_s
+typedef struct bus_s
 {
-char *info, *arg;
+char *content, *arg;
 FILE *file;
 int lifi;
-} lus_t;
-extern lus_t lus;
+} bus_t;
+extern bus_t bus;
 
-
-void free_stack(stack_t *head);
 /**int main(int argc, char *argv[])*/
-void f_nop(stack_t **head, unsigned int counter);
-void f_pop(stack_t **head, unsigned int counter);
-void f_pall(stack_t **head, unsigned int number);
+ssize_t getstdin(char **lineptr, int file);
+char  *clean_line(char *content);
 void f_push(stack_t **head, unsigned int number);
+void f_pall(stack_t **head, unsigned int number);
 void f_pint(stack_t **head, unsigned int number);
-void addnode(stack_t **head, int n);
-void f_add(stack_t **head, unsigned int counter);
+char *_realloc(char *ptr, unsigned int old_size, unsigned int new_size);
 void addqueue(stack_t **head, int n);
+void f_mod(stack_t **head, unsigned int counter);
+void f_pchar(stack_t **head, unsigned int counter);
+void f_pstr(stack_t **head, unsigned int counter);
+void f_rotl(stack_t **head, unsigned int counter);
+void f_rotr(stack_t **head, __attribute__((unused)) unsigned int counter);
+void addnode(stack_t **head, int n);
+int execute(char *content, stack_t **head, unsigned int counter, FILE *file);
+void free_stack(stack_t *head);
+void f_pop(stack_t **head, unsigned int counter);
+void f_swap(stack_t **head, unsigned int counter);
+void f_add(stack_t **head, unsigned int counter);
+void f_nop(stack_t **head, unsigned int counter);
+void f_sub(stack_t **head, unsigned int counter);
+void f_div(stack_t **head, unsigned int counter);
+void f_mul(stack_t **head, unsigned int counter);
 void f_queue(stack_t **head, unsigned int counter);
-
+void f_stack(stack_t **head, unsigned int counter);
 #endif
